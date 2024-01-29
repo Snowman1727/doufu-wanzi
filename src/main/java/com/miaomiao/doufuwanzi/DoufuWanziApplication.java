b6dc596051d606217a4000cb43d51c462c0628f5
shangziyan0505@163.com <shangziyan0505@163.com>
2024-01-29 17:07:48 +0800

    init: 项目初始化

---
+++ src/main/java/com/miaomiao/doufuwanzi/DoufuWanziApplication.java
@@ -0,0 +1,13 @@
+package com.miaomiao.doufuwanzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoufuWanziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoufuWanziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miaomiao/doufuwanzi/DoufuWanziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaomiao.doufuwanzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoufuWanziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
