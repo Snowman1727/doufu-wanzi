e13cb6eb80c56045a4a6dff0375eb48963429243
shangziyan0505@163.com <shangziyan0505@163.com>
2024-03-06 17:50:44 +0800

    feat: 新增用户信息查询接口

--- src/main/java/com/miaomiao/doufuwanzi/DoufuWanziApplication.java
+++ src/main/java/com/miaomiao/doufuwanzi/DoufuWanziApplication.java
@@ -3,17 +3,23 @@ package com.miaomiao.doufuwanzi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @ServletComponentScan
 @MapperScan("com.miaomiao.doufuwanzi.mapper")
-public class DoufuWanziApplication {
+public class DoufuWanziApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DoufuWanziApplication.class, args);
     }
 
+    @Override //为了打包springboot项目
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 }
